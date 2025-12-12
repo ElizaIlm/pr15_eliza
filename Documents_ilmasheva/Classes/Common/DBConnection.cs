1ac1931390a36e1eaa19e7551d6e7d3e163fa365
ilmasheva-eliza <ilmeliza36@gmai.com>
2025-12-12 15:05:12 +0500

    Реализованные классы. ilmasheva

---
+++ Documents_ilmasheva/Classes/Common/DBConnection.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Documents_ilmasheva.Classes.Common
+{
+    public class DBConnect
+    {
+        public static readonly string Path = @"C:\Users\1\OneDrive\Desktop\Авик\Ощепков\МДК 05.02\ПР15\Database.accdb";
+        public static OleDbConnection Connection()
+        {
+            OleDbConnection oleDBConnection = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0; Data Source=" + Path);
+            oleDBConnection.Open();
+            return oleDBConnection;
+        }
+        public static OleDbDataReader Query(string Query, OleDbConnection Connection)
+        {
+            return new OleDbCommand(Query, Connection).ExecuteReader();
+        }
+        public static void CloseConnection(OleDbConnection Connection)
+        {
+            Connection.Close();
+        }
+    }
+}
---
+++ Documents_ilmasheva/Interfaces/IDocument.cs
@@ -0,0 +1,16 @@
+﻿using Documents_ilmasheva.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Documents_ilmasheva.Interfaces
+{
+    public interface IDocument
+    {
+        void Save(bool Update = false);
+        List<Classes.DocumentContext> AllDocuments();
+        void Delete();
+    }
+}
